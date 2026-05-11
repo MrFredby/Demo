5a78d64b288e625944422fcb604abb8ebb63abdb
MrFredby <fredyomarhernandezalba@gmail.com>
2026-05-11 11:25:48 -0600

    Agregar cambios en el backend

--- backend/db.js
+++ backend/db.js
@@ -5,10 +5,11 @@ const conexion = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
-conexion.connect ((err) => {
+conexion.connect((err) => {
     if (err) {
         console.error('Error conectando a MySQL ' + err.stack);
         return;
